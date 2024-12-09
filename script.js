76eafa66c0edbd608b3dfeccc63c9f5e1f47967a
AndelKaz <149527003+AndelKaz@users.noreply.github.com>
2024-12-09 20:36:09 +0200

    Half-final commit

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
-/*
- * Напиши скрипт пошуку самого маленького числа в масиві,
- * при умові, що числа унікальні (не повторюються).
- */
+// // Створити масив з трьох чисел. Змінити значення другого елемента масиву на 10.
+// const numbers = [3, 1, 13];
+// numbers[1] = 10;
+// console.log(numbers);
 
-const number = [10, 6, 8, 22, 54, 28, 12, 9]
-console.log(Math.min(...number));
+// Створити масив із трьох рядків. Додати до масиву ще одну рядків.
+
+// const strings = ["Hi", "Hello", "Good morning"];
+// strings[3] = "Good night";
+// console.log(strings);
+
+// Створити скрипт який поверне суму всіх чисел в масиві.
+
+// const prices = [15, 62, 49, 14, 23]
+// let sum = 0;
+// for (const plural of prices) {
+//     sum += plural
+// }
+// console.log(sum);
+
+// Створити масив з 5-ти чисел. Вивести на екран всі елементи масиву за допомогою циклу for.
+
+// const numbers = [16, 32, 64, 128, 256];
+// let number = 0;
+// for (const number of numbers) {
+//     console.log(number);
+// }
+
+// Створити масив із 5-ти рядків. Вивести на екран кожен рядокз масиву, який містить більше 5-ти символів.
+
+// const strings = ["March", "April", "May", "June", "July"];
+// for (const symbols of strings) {
+//     if (symbols.length >= 5) {
+//         console.log(symbols);
+//     }
+// }
+
+// Створити масив з 10-ти чисел. Знайти та вивести на екран максимальне значення з масиву.
+                 
+
+// const numbers = [16, 165, 72, 91, 5, 19, 1, 9, 163, 63];
+// for(const number of numbers){
+//     if(number)
+// } 
+
+// Створити масив з 10-ти чисел. Знайти всі парні числа в масиві та вивести їх на екран.
+
+const numberі = [16, 165, 72, 91, 5, 19, 1, 9, 163, 63]
+for (const number of numbers) {
+    if (number % 2 === 0 ) {
+        console.log(number);
+    }
+}
